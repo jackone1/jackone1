d14a473540d8b53c01a5a79ffb0be4cc2f2f386c
jackone1 <zheshiwodeshibushi@163.com>
2016-08-14 21:23:38 +0800

    session-share

---
+++ session-share/session-share/session-web1/src/main/java/com/test/demo/SessionDemo1.java
@@ -0,0 +1,39 @@
+package com.test.demo;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SessionDemo1
+ */
+public class SessionDemo1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SessionDemo1() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("username: " + request.getSession().getAttribute("username"));
+		
+		
+	}
+
+}
